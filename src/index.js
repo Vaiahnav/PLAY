62734d058acf59ed9f5f6e01ec698ea4bba9ea1f
vaishnav <vanshparadkar00@gmail.com>
2026-07-15 21:01:35 +0530

    server connection

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
 
 connectDB()
+    .then(() => {
+        const server = app.listen(process.env.PORT || 5000 , () => {
+            console.log(`server is running at port ${process.env.PORT}`);
+        })
 
- 
+        server.on("error", (error) =>{
+            console.log("server error : ", error)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection  failed", err)
+        throw err
+    })
 
 
 
 
- 
+
+
+
 
 // import express from "express"
 // const app = express();
